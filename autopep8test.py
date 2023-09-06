7b63c1c2e1c7b79e2e5ea0607c3946e712be1741
Alexander Mittet <alexandermittet@gmail.com>
2023-09-06 22:22:33 +0200

    added example file

---
+++ autopep8test.py
@@ -0,0 +1,22 @@
+"""You need the vscode extenstion "balck formatter" for this to work."""
+
+
+def function_name(
+    input_str: str, another_rgument_thsiiswaytoo_long: str
+) -> None:  # See above, it cut the line here
+    """Docstring...
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+    Attributes:
+
+        attr1 (int): Description of attr1x
+
+            attr2 (str): Description of attr2
+    Retu
+    rns:
+        int: Description of return value"""
+
+    print("Hello world!" + input_str)
+    print(another_rgument_thsiiswaytoo_long)
