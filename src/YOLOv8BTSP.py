dd713dc2e39751d5fb38882f830690cdbdf9ce2e
Marcusnsr <mnsr@hotmail.dk>
2023-09-25 12:30:06 +0200

    moved

---
+++ src/YOLOv8BTSP.py
@@ -0,0 +1,28 @@
+import ultralytics
+import supervision
+import numpy as np
+from ByteTrack.yolox.tracker.byte_tracker import BYTETracker, STrack
+from onemetric.cv.utils.iou import box_iou_batch
+from dataclasses import dataclass
+from supervision.draw.color import ColorPalette
+from supervision.geometry.dataclasses import Point
+from supervision.video.dataclasses import VideoInfo
+from supervision.video.source import get_video_frames_generator
+from supervision.video.sink import VideoSink
+from supervision.notebook.utils import show_frame_in_notebook
+from supervision.tools.detections import Detections, BoxAnnotator
+from supervision.tools.line_counter import LineCounter, LineCounterAnnotator
+from typing import List
+
+np.float = float
+
+
+# ByteTrack args
+@dataclass(frozen=True)
+class BYTETrackerArgs:
+    track_thresh: float = 0.25
+    track_buffer: int = 30
+    match_thresh: float = 0.8
+    aspect_ratio_thresh: float = 3.0
+    min_box_area: float = 1.0
+    mot20: bool = False
